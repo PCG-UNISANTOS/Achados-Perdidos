6e389c998f911da87fc02c7989303c72b9bbda69
João Falcão <jgabfalcao@gmail.com>
2025-05-06 20:22:37 -0300

    Feat: Criando o componente Listagem

--- app/src/components/Header/Header.jsx
+++ app/src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import '../../assets/styles/_reset.css';
 import '../../assets/styles/_variables.css';
 import '../../assets/styles/index.css';
+import './Header.css'
 
 
 function Header() {
@@ -10,13 +11,16 @@ function Header() {
 
   return (
     <>
-      <header>
-        <ul>
-            <li>Home</li>
-            <li>Achados</li>
-            <li>Perdidos</li>
-        </ul>
-        </header>
+      <header className='header-section'>
+        <div className='header-section-container'>
+          <ul className='header-section-container-lista'>
+              <li className='header-section-container-lista__item'><a href='/'>
+                Home</a></li>
+              <li className='header-section-container-lista__item'><a href='/'>
+                Listagem</a></li>
+          </ul>
+        </div>
+      </header>
     </>
   )
 }
