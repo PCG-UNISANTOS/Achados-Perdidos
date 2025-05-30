b692eb5255a8edd2b8097504f250157bfaccd1df
João Falcão <jgabfalcao@gmail.com>
2025-05-29 21:44:13 -0300

    Ajustando o componente de Header

--- app/src/components/Header/Header.jsx
+++ app/src/components/Header/Header.jsx
@@ -1,28 +1,29 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import '../../assets/styles/_reset.css';
-import '../../assets/styles/_variables.css';
-import '../../assets/styles/index.css';
-import './Header.css'
-
+import "../../assets/styles/_reset.css";
+import "../../assets/styles/_variables.css";
+import "../../assets/styles/index.css";
+import "./Header.css";
 
 function Header() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <header className='header-section'>
-        <div className='header-section-container'>
-          <ul className='header-section-container-lista'>
-              <li className='header-section-container-lista__item'><a href='/'>
-                Home</a></li>
-              <li className='header-section-container-lista__item'><a href='/'>
-                Listagem</a></li>
+      <header className="header-section">
+        <div className="header-section-container">
+          <ul className="header-section-container-lista">
+            <li className="header-section-container-lista__item">
+              <a href="/">Home</a>
+            </li>
+            <li className="header-section-container-lista__item">
+              <a href="/">Listagem</a>
+            </li>
           </ul>
         </div>
       </header>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
