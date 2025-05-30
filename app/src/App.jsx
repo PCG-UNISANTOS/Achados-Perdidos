fcc9ed822301ede3770f15c0d14f0487bbdf4b8b
João Falcão <jgabfalcao@gmail.com>
2025-05-29 21:43:50 -0300

    Inserindo biblioteca para Navegação

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,19 +1,24 @@
-import { useState } from 'react'
-import Header from './components/Header/Header';
-import Listagem from './components/Listagem/Listagem';
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Listagem from "./components/Listagem/Listagem";
+import Visualizar from "./components/Visualizar/Visualizar";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <main>
-      <Header/>
-      <Listagem/>
-    </main>
+      <main>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Listagem />} />
+            <Route path="/visualizar" element={<Visualizar />} />
+          </Routes>
+        </BrowserRouter>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
