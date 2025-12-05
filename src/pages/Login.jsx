2395a5903f2a0b3b82288229a0bd6fc547916874
João Falcão <jgabfalcao@gmail.com>
2025-12-05 16:30:31 -0300

    Feat: Incluindo formulário de Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,18 @@
-import logo from "../Arquitetura/Icones/logo/logo.png";
+import { useForm, Controller } from "react-hook-form";
+import { IMaskInput } from "react-imask";
 import Header from "../components/Header";
 
 export default function Login() {
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  function onSubmit(data) {
+    console.log("Dados enviados:", data);
+  }
   return (
     <>
       <Header />
@@ -12,7 +23,52 @@ export default function Login() {
           </div>
 
           <div className="login-section-container__content">
-            <form action=""></form>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              {/* CPF */}
+              <label>CPF</label>
+              <Controller
+                name="cpf"
+                control={control}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <IMaskInput
+                    {...field}
+                    mask="000.000.000-00"
+                    unmask={false}
+                    placeholder="000.000.000-00"
+                  />
+                )}
+              />
+
+              {/* Telefone */}
+              <label>Telefone</label>
+              <Controller
+                name="telefone"
+                control={control}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <IMaskInput
+                    {...field}
+                    mask="(00) 00000-0000"
+                    unmask={false}
+                    placeholder="(00) 00000-0000"
+                  />
+                )}
+              />
+
+              {/* Email */}
+              <label>Email</label>
+              <input type="email" {...register("email", { required: true })} />
+
+              {/* Senha */}
+              <label>Senha</label>
+              <input
+                type="password"
+                {...register("senha", { required: true })}
+              />
+
+              <button type="submit">Entrar</button>
+            </form>
           </div>
         </div>
       </section>
